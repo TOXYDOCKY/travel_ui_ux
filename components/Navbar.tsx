f5bd284b0da9d8ef8f7984147e420cc52498a3c2
TOXYDOCKY <dockyrsb@gmail.com>
2025-05-05 09:55:38 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,9 +6,6 @@ import Button from "./Button"
 const Navbar = () => {
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
-      <Link href="/">
-        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
-      </Link>
 
       <ul className="hidden h-full gap-12 lg:flex">
         {NAV_LINKS.map((link) => (
@@ -17,15 +14,6 @@ const Navbar = () => {
           </Link>
         ))}
       </ul>
-
-      <div className="lg:flexCenter hidden">
-        <Button 
-          type="button"
-          title="Login"
-          icon="/user.svg"
-          variant="btn_dark_green"
-        />
-      </div>
 
       <Image 
         src="menu.svg"
