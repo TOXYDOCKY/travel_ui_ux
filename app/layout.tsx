04b5f5cf3d311fccb458e860957e349ca5b9ce09
TOXYDOCKY <dockyrsb@gmail.com>
2025-05-05 09:52:18 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
-  title: 'Travel',
-  description: 'Travel UI/UX App for Camping',
+  title: 'Manuel y Carlos',
+  description: '',
 }
 
 export default function RootLayout({
