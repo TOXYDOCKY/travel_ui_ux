d4a3addd91ae3e345dedcc91fde9da0c511e339f
TOXYDOCKY <dockyrsb@gmail.com>
2025-05-07 09:41:05 +0200

    Update Camp.tsx

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -53,15 +53,15 @@ const Camp = () => {
       <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
         <CampSite 
           backgroundImage="bg-bg-img-1"
-          title="Putuk Truno Camp"
-          subtitle="Prigen, Pasuruan"
+          title="Asentamiento Putuk Truno"
+          subtitle="Siberia, Rusia"
           peopleJoined="50+ Joined"
         />
         <CampSite 
           backgroundImage="bg-bg-img-2"
-          title="Mountain View Camp"
-          subtitle="Somewhere in the Wilderness"
-          peopleJoined="50+ Joined"
+          title="Vista del Asentamiento en la Montaña"
+          subtitle="Algun lugar en Siberia"
+          peopleJoined="50+ Aventureros"
         />
       </div>
 
