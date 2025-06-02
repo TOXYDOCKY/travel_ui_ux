4c0bec5aa0a15715c39396b68a411cec567156b9
TOXYDOCKY <dockyrsb@gmail.com>
2025-06-02 09:37:26 +0200

    Update Features.tsx

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,29 +1,6 @@
-// components/Features.tsx
-import { FEATURES } from '@/constants'
+mport { FEATURES } from '@/constants'
 import Image from 'next/image'
 import React from 'react'
-
-type FeatureItemProps = {
-  title: string;
-  icon: string;
-  description: string;
-}
-
-const FeatureItem = ({ title, icon, description }: FeatureItemProps) => {
-  return (
-    <li className="flex w-full flex-1 flex-col items-start">
-      <div className="rounded-full p-4 lg:p-7 bg-green-50">
-        <Image src={icon} alt="feature icon" width={28} height={28} />
-      </div>
-      <h2 className="bold-20 lg:bold-32 mt-5 capitalize">
-        {title}
-      </h2>
-      <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">
-        {description}
-      </p>
-    </li>
-  )
-}
 
 const Features = () => {
   return (
@@ -40,7 +17,7 @@ const Features = () => {
         </div>
 
         <div className="z-20 flex w-full flex-col lg:w-[60%]">
-          <div className="relative">
+          <div className='relative'>
             <Image
               src="/camp.svg"
               alt="camp"
@@ -48,14 +25,13 @@ const Features = () => {
               height={50}
               className="absolute left-[-5px] top-[-28px] w-10 lg:w-[50px]"
             />
-            <h2 className="bold-40 lg:bold-64">Porque somos únicos</h2>
+            <h2 className="bold-40 lg:bold-64">Our Features</h2>
           </div>
-
           <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mg-20 lg:gap-20">
             {FEATURES.map((feature) => (
-              <FeatureItem
+              <FeatureItem 
                 key={feature.title}
-                title={feature.title}
+                title={feature.title} 
                 icon={feature.icon}
                 description={feature.description}
               />
@@ -64,6 +40,28 @@ const Features = () => {
         </div>
       </div>
     </section>
+  )
+}
+
+type FeatureItem = {
+  title: string;
+  icon: string;
+  description: string;
+}
+
+const FeatureItem = ({ title, icon, description }: FeatureItem) => {
+  return (
+    <li className="flex w-full flex-1 flex-col items-start">
+      <div className="rounded-full p-4 lg:p-7 bg-green-50">
+        <Image src={icon} alt="map" width={28} height={28} />
+      </div>
+      <h2 className="bold-20 lg:bold-32 mt-5 capitalize">
+        {title}
+      </h2>
+      <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">
+        {description}
+      </p>
+    </li>
   )
 }
 
