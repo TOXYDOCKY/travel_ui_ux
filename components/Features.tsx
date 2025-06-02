de46e33b487e62a8fca8a598e615a0dc816c6508
TOXYDOCKY <dockyrsb@gmail.com>
2025-06-02 09:34:55 +0200

    Update Features.tsx

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,14 +1,28 @@
+// components/Features.tsx
 import { FEATURES } from '@/constants'
 import Image from 'next/image'
 import React from 'react'
-import React, { Dispatch, SetStateAction, useEffect, useRef } from 'react'
 
-type Props = {
-    question: string,
-    answer: string,
-    turn: boolean[],
-    setTurn: Dispatch<SetStateAction<boolean[]>>,
-    idx: number
+type FeatureItemProps = {
+  title: string;
+  icon: string;
+  description: string;
+}
+
+const FeatureItem = ({ title, icon, description }: FeatureItemProps) => {
+  return (
+    <li className="flex w-full flex-1 flex-col items-start">
+      <div className="rounded-full p-4 lg:p-7 bg-green-50">
+        <Image src={icon} alt="feature icon" width={28} height={28} />
+      </div>
+      <h2 className="bold-20 lg:bold-32 mt-5 capitalize">
+        {title}
+      </h2>
+      <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">
+        {description}
+      </p>
+    </li>
+  )
 }
 
 const Features = () => {
@@ -26,7 +40,7 @@ const Features = () => {
         </div>
 
         <div className="z-20 flex w-full flex-col lg:w-[60%]">
-          <div className='relative'>
+          <div className="relative">
             <Image
               src="/camp.svg"
               alt="camp"
@@ -34,13 +48,14 @@ const Features = () => {
               height={50}
               className="absolute left-[-5px] top-[-28px] w-10 lg:w-[50px]"
             />
-            <h2 className="bold-40 lg:bold-64">Porque somos Unicos</h2>
+            <h2 className="bold-40 lg:bold-64">Porque somos únicos</h2>
           </div>
+
           <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mg-20 lg:gap-20">
             {FEATURES.map((feature) => (
-              <FeatureItem 
+              <FeatureItem
                 key={feature.title}
-                title={feature.title} 
+                title={feature.title}
                 icon={feature.icon}
                 description={feature.description}
               />
@@ -48,64 +63,7 @@ const Features = () => {
           </ul>
         </div>
       </div>
-      const contentRef = useRef<HTMLDivElement>(null);
-
-    useEffect(()=>{
-        if(contentRef.current){
-            contentRef.current.style.maxHeight = turn![idx] ? `${contentRef.current.scrollHeight}px`:"0px"
-        }
-
-    },[contentRef, turn, idx])
-
-    const toggleAccordion = () => {
-        let newTurn = [...turn!]
-        newTurn[idx] = !newTurn[idx]
-        setTurn!(newTurn)
-    }
-    <div className='flex flex-col items-center justify-center w-full px-2 text-lg pt-4 lg:text-base'>
-      <button onClick={toggleAccordion}
-       className={`bg-transparent px-5 shadow cursor-pointer w-full h-full ${turn![idx]}`}>
-        <div className='py-3'>
-           <div className='flex items-center justify-between h-14 text-left'>
-              <span className='ml-2 font-medium lg:font-semibold lg:text-xl text-sm text-sky-600'>{question}</span>
-              <div>
-               { turn![idx] ? <Image src={minus} alt="" width={20} height={20}/> :
-                <Image src={plus} alt="" width={20} height={20}/>}
-
-              </div>
-           </div>
-           <div ref={contentRef} className='mx-4 overflow-hidden text-left transition-all duration-500 h-full'>
-              <p className='py-1 font-normal leading-normal text-justify whitespace-pre-line text-xs lg:text-lg'>
-                {answer}
-              </p>
-           </div>
-        </div>
-      </button>
-    </div>
-  
     </section>
-  )
-}
-
-type FeatureItem = {
-  title: string;
-  icon: string;
-  description: string;
-}
-
-const FeatureItem = ({ title, icon, description }: FeatureItem) => {
-  return (
-    <li className="flex w-full flex-1 flex-col items-start">
-      <div className="rounded-full p-4 lg:p-7 bg-green-50">
-        <Image src={icon} alt="map" width={28} height={28} />
-      </div>
-      <h2 className="bold-20 lg:bold-32 mt-5 capitalize">
-        {title}
-      </h2>
-      <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">
-        {description}
-      </p>
-    </li>
   )
 }
 
